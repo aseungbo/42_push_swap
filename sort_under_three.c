59cf7002680d6e4fd734f202d9c8a691ce1ef4b4
silica67sdm <1127lse@gmail.com>
2021-07-01 16:55:46 +0900

    sort_three 함수 정리

--- sort_under_three.c
+++ sort_under_three.c
@@ -20,18 +20,8 @@ int sort_three(t_stack **head)
     t_stack *n2 = n1->next;
     t_stack *n3 = n2->next;
 
-    // printf("\n------ before ------\n");
-    // printf("n1: %d\n", n1->num);
-    // printf("n2: %d\n", n2->num);
-    // printf("n3: %d\n", n3->num);
-
-    if (n1->num < n2->num && n2->num < n3->num)
+    if (n1->num < n3->num && n3->num < n2->num)
     {
-        // puts("\n case1");
-    }
-    else if (n1->num < n3->num && n3->num < n2->num)
-    {
-        // puts("\n case2");
         rrab(head);
         write(1, "rra\n", 4);
         sab(head);
@@ -39,38 +29,27 @@ int sort_three(t_stack **head)
     }
     else if (n2->num < n1->num && n1->num < n3->num)
     {
-        // puts("\n case3");
         sab(head);
         write(1, "sa\n", 3);
     }
     else if (n2->num < n3->num && n3->num < n1->num)
     {
-        // puts("\n case4");
         rab(head);
         write(1, "ra\n", 3);
     }
     else if (n3->num < n1->num && n1->num < n2->num)
     {
-        // puts("\n case5");
         rrab(head);
         write(1, "rra\n", 4);
     }
     else if (n3->num < n2->num && n2->num < n1->num)
     {
-        // puts("\n case6");
         sab(head);
         write(1, "sa\n", 3);
         rrab(head);
         write(1, "rra\n", 4);
     }
 
-    // n1 = *head;
-    // n2 = n1->next;
-    // n3 = n2->next;
-    // printf("\n------ after ------\n");
-    // printf("n1: %d\n", n1->num);
-    // printf("n2: %d\n", n2->num);
-    // printf("n3: %d\n", n3->num);
     return (1);
 }
 
